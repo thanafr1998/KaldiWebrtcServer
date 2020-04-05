05b0d61369a6e103a5bd45a307f254a99f6307f7
Thanawat <taan02991@hotmail.com>
2020-04-05 21:45:07 +0700

    Add VueRouter and DefaultRecorder

--- docker/web/frontend/src/main.js
+++ docker/web/frontend/src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import DefaultRecorder from './components/DefaultRecorder'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+const routes = [
+    { path: '/default-recorder', component: DefaultRecorder }
+]
+
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
